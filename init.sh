1ed368114e2b0e750f85dd7a23647512a578e746
tongwai <anthony@voicetube.com>
2020-09-17 11:35:27 +0800

    [chore] update dot env

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 echo 'decrypt .env file'
 gcloud beta secrets versions access latest --secret=vt-etl-agent --project=voicetube-test > "${DIR}/.service_acount.json"
+gcloud beta secrets versions access latest --secret=tootkit-dot-env --project=voicetube-test > "${DIR}/.env"
